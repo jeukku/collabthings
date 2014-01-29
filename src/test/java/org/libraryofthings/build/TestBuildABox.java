dc856234d46bb308669a4cafa310158d350d7723
jeukku <juuso@vilmunen.net>
2014-01-29 22:30:03 +0200

    Build a box. Useless currently.

--- src/test/java/org/libraryofthings/build/TestBuildABox.java
+++ src/test/java/org/libraryofthings/build/TestBuildABox.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.libraryofthings.LOTEnvironment;
 import org.libraryofthings.LOTTestCase;
+import org.libraryofthings.RunEnvironment;
 import org.libraryofthings.math.LVector;
 import org.libraryofthings.model.LOTPart;
 import org.libraryofthings.model.LOTScript;
@@ -11,9 +12,11 @@ import org.libraryofthings.model.LOTSubPart;
 import org.libraryofthings.model.LOTTool;
 import org.xml.sax.SAXException;
 
+import waazdoh.cutils.MID;
+
 public class TestBuildABox extends LOTTestCase {
 
-	public void testClient() throws IOException, SAXException {
+	public void testBox() throws IOException, SAXException {
 		LOTEnvironment env = getNewEnv();
 		assertNotNull(env);
 
@@ -30,7 +33,7 @@ public class TestBuildABox extends LOTTestCase {
 		}
 
 		box.getSubParts().get(0)
-				.setOrientation(new LVector(0, -1, 0), new LVector(0, -1, 0));
+				.setOrientation(new LVector(0, -1, 0), new LVector(0, 1, 0));
 		box.getSubParts().get(1)
 				.setOrientation(new LVector(-1, 0, 0), new LVector(-1, 0, 0));
 		box.getSubParts().get(2)
@@ -43,24 +46,48 @@ public class TestBuildABox extends LOTTestCase {
 				.setOrientation(new LVector(0, 1, 0), new LVector(0, 1, 0));
 
 		// TODO picking up plates, moving them and leaving them somewhere
-		/// Kirjoita se auki, perkele!!!!.
-		
+
 		// Create a tool to pick up plates
 		LOTTool tool = new LOTTool(env);
 
 		//
 		LOTScript assembyscript = getAssemblyScript(tool, box, env);
-		
+
+		LOTPart destinationpart = new LOTPart(env);
+
+		/*
+		RunEnvironment runenv = SimualtionEnvironment(env);
+		runenv.setParameter("partid", box.getServiceObject().getID());
+		runenv.addPart("destinationpart", destinationpart);
+
+		assembyscript.run(runenv);
+
 		// TODO make it a box.
 		assertTrue("is a box?", false);
+		*/
+	}
+
+	private RunEnvironment SimualtionEnvironment(LOTEnvironment env) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	private LOTScript getAssemblyScript(LOTTool tool, LOTPart box,
 			LOTEnvironment env) {
 		String s = "";
+		s += "function run() { ";
+		s += "	part = getEnv().getPart(getParameters('partid'));";
+		s += "	destinationpart = getEnv().getPart(getParameters('destinationpartid'));";
+		//
+		s += "  part.getSubParts().foreach(function(subpart) {";
+		s += "     moveAndAttach(subpart, destinationpart)";
+		s += "  });";
+		s += "}";
+
 		//
 		LOTScript lots = new LOTScript(env);
 		lots.setScript(s);
+
 		return lots;
 	}
 }
