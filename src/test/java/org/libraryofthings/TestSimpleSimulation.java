7a328f3695200f62cf919848ce917792d32ec60f
jeukku <juuso@vilmunen.net>
2014-03-19 16:33:15 +0200

    Testing failing script in simplesimulation.

---
+++ src/test/java/org/libraryofthings/TestSimpleSimulation.java
@@ -0,0 +1,24 @@
+package org.libraryofthings;
+
+import java.io.IOException;
+
+import org.libraryofthings.model.LOTScript;
+import org.libraryofthings.simulation.LOTSimpleSimulation;
+import org.libraryofthings.simulation.LOTSimulation;
+import org.libraryofthings.simulation.LOTSimulationEnvironment;
+import org.xml.sax.SAXException;
+
+public class TestSimpleSimulation extends LOTTestCase {
+
+	public void testFailingScript() throws IOException, SAXException {
+		LOTEnvironment env = getNewEnv();
+
+		LOTScript s = new LOTScript(env);
+		s.setScript("function test() {}");
+
+		RunEnvironment runenv = new LOTSimulationEnvironment(env);
+		runenv.addTask(s, "test_fail");
+		LOTSimulation simulation = new LOTSimpleSimulation(runenv);
+		assertFalse(simulation.run());
+	}
+}
