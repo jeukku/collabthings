57343a1f2a464e98dbd04f1bd32e2dc2b42631b2
Juuso Vilmunen <juuso@vilmunen.net>
2014-07-20 18:19:57 +0300

    syncronized lists and method to wait until pool is not empty.

--- src/main/java/org/libraryofthings/environment/LOTPool.java
+++ src/main/java/org/libraryofthings/environment/LOTPool.java
@@ -5,26 +5,56 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import org.libraryofthings.LLog;
 import org.libraryofthings.model.LOTPart;
 
 public class LOTPool {
 	private Map<String, List<LOTPart>> parts = new HashMap<>();
+	private LLog log = LLog.getLogger(this);
 
-	public LOTPart getPart(String string) {
-		List<LOTPart> list = parts.get(string);
-		if (list != null && !list.isEmpty()) {
-			return list.remove(0);
-		} else {
-			return null;
+	public synchronized LOTPart getPart(String string) {
+		try {
+			List<LOTPart> list = parts.get(string);
+			if (list != null && !list.isEmpty()) {
+				return list.remove(0);
+			} else {
+				return null;
+			}
+		} finally {
+			this.notifyAll();
 		}
 	}
 
-	public void addPart(String pool, LOTPart p) {
+	public synchronized void addPart(String pool, LOTPart p) {
 		List<LOTPart> list = parts.get(pool);
 		if (list == null) {
 			list = new LinkedList<LOTPart>();
 			parts.put(pool, list);
 		}
 		list.add(p);
+		//
+		this.notifyAll();
+	}
+
+	public synchronized void waitForPart(String pool) {
+		if (isPartPoolEmpty(pool)) {
+			log.info("waiting for part " + pool);
+		}
+
+		while (isPartPoolEmpty(pool)) {
+			waitABit();
+		}
+	}
+
+	private boolean isPartPoolEmpty(String pool) {
+		return parts.get(pool) == null || parts.get(pool).isEmpty();
+	}
+
+	private synchronized void waitABit() {
+		try {
+			this.wait();
+		} catch (InterruptedException e) {
+			LLog.getLogger(this).error(this, "wait", e);
+		}
 	}
 }
