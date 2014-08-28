31e837ce643357fa6ba20d96f7d07d26261988d3
Juuso Vilmunen <juuso@vilmunen.net>
2014-08-28 15:59:43 +0300

    Support for info test in LLog (written on every line).

--- src/main/java/org/libraryofthings/LLog.java
+++ src/main/java/org/libraryofthings/LLog.java
@@ -6,13 +6,20 @@ import java.util.logging.Logger;
 
 public class LLog {
 	private Logger log;
+	private String info = "";
+	private int maxinfolength = 40;
 
 	LLog(Object o) {
 		log = Logger.getLogger("" + o);
+		setInfo("" + o);
 	}
 
 	public void info(String string) {
-		log.info(string);
+		log.info(getLine(string));
+	}
+
+	private String getLine(String string) {
+		return "[" + info + "] " + string;
 	}
 
 	public void error(Object o, String sourceMethod, Throwable e1) {
@@ -29,7 +36,22 @@ public class LLog {
 	}
 
 	public void fine(String string) {
-		log.info(string);
+		log.info(getLine(string));
+	}
+
+	public LLog instance(Object o) {
+		return LLog.getLogger(o);
+	}
+
+	public void setInfo(final String ninfo) {
+		this.info = ninfo;
+		if (info.length() > maxinfolength) {
+			info = info.substring(0, maxinfolength);
+		} else {
+			while (info.length() < maxinfolength) {
+				info = info + " ";
+			}
+		}
 	}
 
 }
