4ee73a891665ec6ee6732f39a6de685562c8130a
Juuso Vilmunen <juuso@vilmunen.net>
2014-09-01 09:12:00 +0300

    Testcase for ReallySimp[leSuperheroRobot.

---
+++ src/test/java/org/libraryofthings/unittests/TestSuperheroRobot.java
@@ -0,0 +1,56 @@
+package org.libraryofthings.unittests;
+
+import java.io.IOException;
+
+import org.libraryofthings.LOTClient;
+import org.libraryofthings.LOTTestCase;
+import org.libraryofthings.environment.LOTRunEnvironmentImpl;
+import org.libraryofthings.environment.LOTToolState;
+import org.libraryofthings.environment.RunEnvironment;
+import org.libraryofthings.integrationtests.ReallySimpleSuperheroRobot;
+import org.libraryofthings.math.LVector;
+import org.libraryofthings.model.LOTEnvironmentImpl;
+import org.libraryofthings.model.LOTTool;
+import org.xml.sax.SAXException;
+
+import waazdoh.util.MTimedFlag;
+
+public final class TestSuperheroRobot extends LOTTestCase {
+
+	private static final double MAX_DISTANCE = 0.00001;
+
+	public void testMoveTo() throws IOException, SAXException {
+		final LOTClient e = getNewClient();
+		assertNotNull(e);
+
+		RunEnvironment rune = new LOTRunEnvironmentImpl(e,
+				new LOTEnvironmentImpl(e));
+		final ReallySimpleSuperheroRobot robot = new ReallySimpleSuperheroRobot(
+				rune);
+		LOTToolState lotToolState = new LOTToolState("test", rune, new LOTTool(
+				e), null);
+		robot.setTool(lotToolState);
+
+		final MTimedFlag flag = new MTimedFlag(3000);
+
+		new Thread(() -> {
+			try {
+				while (rune.isRunning() && !flag.isTriggered()) {
+					robot.step(10);
+				}
+			} finally {
+				rune.stop();
+				e.stop();
+			}
+		}).start();
+
+		LVector targetlocation = new LVector(10, 0, 0);
+		robot.move(targetlocation, new LVector(0, 1, 0));
+
+		assertTrue(
+				"" + targetlocation + " " + robot.getLocation(),
+				targetlocation.getSub(robot.getLocation()).length() < MAX_DISTANCE);
+
+		rune.stop();
+	}
+}
