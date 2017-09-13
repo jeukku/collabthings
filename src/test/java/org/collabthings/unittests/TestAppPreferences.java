c0a6961805a85ff434ab6e057568ff6be7886176
Juuso Vilmunen <juuso@vilmunen.net>
2017-09-13 08:57:22 +0300

    testing AppPreferences

---
+++ src/test/java/org/collabthings/unittests/TestAppPreferences.java
@@ -0,0 +1,48 @@
+package org.collabthings.unittests;
+
+import java.util.UUID;
+
+import org.collabthings.CTTestCase;
+import org.collabthings.app.AppPreferences;
+
+public final class TestAppPreferences extends CTTestCase {
+
+	public void testDefaultValue() {
+		AppPreferences p = getPreferences();
+		String testvalue = "testvalue_" + UUID.randomUUID();
+		assertEquals(testvalue, p.get("testvalue/test/2", testvalue));
+		
+		assertEquals(p.get("resource.value", "none"), "hello");
+	}
+
+	public void testWrite() {
+		AppPreferences p = getPreferences();
+		String testvalue = "testvalue_" + UUID.randomUUID();
+		String name = "testvalue/test/" + UUID.randomUUID();
+		p.set(name, testvalue);
+		assertEquals(testvalue, p.get(name, "FAIL_" + UUID.randomUUID()));
+	}
+
+	public void testBoolean() {
+		AppPreferences p = getPreferences();
+		String name = "testvalue/test/" + UUID.randomUUID();
+		p.set(name, false);
+		assertFalse(p.getBoolean(name, true));
+		p.set(name, true);
+		assertTrue(p.getBoolean(name, false));
+	}
+
+	public void testDouble() {
+		AppPreferences p = getPreferences();
+		String name = "testvalue/test/" + UUID.randomUUID();
+		p.set(name, "1.0");
+		assertEquals(1.0, p.getDouble(name, 2.0));
+		assertEquals(3.0, p.getDouble(name + "new", 3.0));
+	}
+
+	private AppPreferences getPreferences() {
+		AppPreferences p = new AppPreferences("testapppreferences/" + UUID.randomUUID());
+		return p;
+	}
+
+}
