1418eeebecf8bb5cd3650dd6749116cf4e43234b
Juuso Vilmunen <juuso@vilmunen.net>
2017-09-13 09:41:16 +0300

    test app closed

--- src/test/java/org/collabthings/unittests/TestCTApp.java
+++ src/test/java/org/collabthings/unittests/TestCTApp.java
@@ -26,11 +26,33 @@ public final class TestCTApp extends CTTestCase {
 		assertTrue(flag.wasTriggerCalled());
 	}
 
+	public void testClient() throws MalformedURLException {
+		CTApp app = new CTTestApp();
+		assertNotNull(app.getWClient());
+		assertNotNull(app.getLClient());
+	}
+
 	public void testAdd() throws IOException, SAXException {
 		CTApp app = new CTTestApp();
 
 		assertNotNull(app.newPart());
 		assertNotNull(app.newFactory());
+	}
+
+	public void testClose() throws IOException, SAXException {
+		CTApp app = new CTTestApp();
+		WTimedFlag flag = new WTimedFlag(4000);
+
+		app.addTask(() -> {
+			flag.trigger();
+		});
+
+		flag.waitTimer();
+
+		app.close();
+		Thread.getAllStackTraces().keySet().forEach(t -> t.interrupt());
+
+		assertTrue(app.isClosed());
 	}
 
 	private class CTTestApp extends CTApp {
