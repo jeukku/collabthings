4a4154342ce51b7836ff684b063dde4b000bb55b
Juuso Vilmunen <juuso@vilmunen.net>
2017-12-29 17:26:41 +0200

    maven deploy

--- src/main/java/org/collabthings/CTEvent.java
+++ src/main/java/org/collabthings/CTEvent.java
@@ -17,18 +17,18 @@ import java.util.List;
 import org.collabthings.model.CTObject;
 
 public class CTEvent {
-	private List<CTObject> os = new LinkedList<>();
+	private List<Object> os = new LinkedList<>();
 	private String info;
 
 	public CTEvent(String string) {
 		this.info = string;
 	}
 
-	public void addHandled(CTObject o) {
+	public void addHandled(Object o) {
 		os.add(o);
 	}
 
-	public boolean isHandled(CTObject o) {
+	public boolean isHandled(Object o) {
 		return os.contains(o);
 	}
 }
